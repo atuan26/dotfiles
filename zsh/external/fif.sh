fe3c4701e1ebaf13ebaedaf4fe22f63eb1499323
atuan26 <natuann26@gmail.com>
2025-08-13 09:31:37 +0700

    feat: :sparkles: update fif function, tmux

--- zsh/external/fif.sh
+++ zsh/external/fif.sh
@@ -1,5 +1,5 @@
 fif() {
     if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
     local file
-    file="$(rga --max-count=1 --ignore-case --files-with-matches --no-messages "$@" | fzf-tmux +m --preview="rga --ignore-case --pretty --context 10 '"$@"' {}")" && open "$file"
+    file="$(rga --max-count=1 --ignore-case --files-with-matches --no-messages "$@" | fzf-tmux +m --preview="rga --ignore-case --pretty --context 10 '"$@"' {}")" && $EDITOR "$file"
 }
